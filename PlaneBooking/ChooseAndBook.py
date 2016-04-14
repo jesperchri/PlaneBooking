78c3ba1a458e989da2e684f92410bf4b19f560e7
JC <jesperchri@hotmail.com>
2016-04-14 21:45:27 +0200

    14-04--2145

--- PlaneBooking/ChooseAndBook.py
+++ PlaneBooking/ChooseAndBook.py
@@ -2,6 +2,7 @@
 
 class Flight:
     
+    """No one needs to know about the aircraft class which simplifies the system"""
     def __init__(self, number, aircraft):
         
         #
@@ -13,7 +14,7 @@ class Flight:
         #
         self._number = number
         self._aircraft = aircraft
-        self._export = aircraft.model
+        self._export = aircraft
         
         #
         rows, seats = self._aircraft.seating_plan()
@@ -35,7 +36,25 @@ class Flight:
         return self._aircraft
 
     def exportcleanvalue(self):
-        return self._export
+        return self._export.model()
+    
+    #Need to return this
+    def returnseating(self):
+        return self._seating()
+
+    def make_boarding_cards(self, card_printer):
+        for passenger, seat in sorted (self._passenger_seats()):
+            card_printer(passenger, seat, self.number(), self.aircraft_model())
+
+
+    def _passenger_seats(self):
+        """An iterable series of passenger seating allocations"""
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row, letter))  
 
 class aircraft:
 
@@ -52,7 +71,27 @@ class aircraft:
         return self._model
 
     def seating_plan(self):
-        return (range(1, self._num_rows + 1), "ABCDEFGHIJK"[:self._num_seets_pr_row])
+        return (range(1, self._num_rows + 1), 
+            "ABCDEFGHIJK"[:self._num_seets_pr_row])
 
-    def export(self):
-        return self._model
+def make_flight():
+    f = Flight("BA758", aircraft("YY8890", "Boeing 747", num_rows=22, num_seets_pr_row=6))
+    f.allocate_seat("12A", "Guido Van Rossum")
+    f.allocate_seat("15F", "Bjarne Stroustrup")
+    f.allocate_seat("15E", "Anders Hejlsberg")
+    f.allocate_seat("1c", "John McCarty")
+    f.allocate_seat("1d", "Richard Hickey")
+    return f
+
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = " | Name:   {0}"   \
+             "   Flight: {1}"   \
+             "   Seat:   {2}"   \
+             "   Aircraft{3}"   \
+             " |".format(passenger, flight_number, seat, aircraft)
+    banner = '+' + '-' * (len(output) - 2) + '+'
+    border = '|' + ' ' * (len(output) - 2) + '|'
+    lines = [banner, border, output, border, banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()
--- PlaneBooking/PlaneBooking.py
+++ PlaneBooking/PlaneBooking.py
@@ -5,10 +5,14 @@ from BoardingCardCreate import *
 
 
 #Create plane
-info = Flight("JJ8989", aircraft("YY8890", "Boeing 747", num_rows = 21, num_seets_pr_row = 6))
+info = Flight("JJ8989", aircraft("YY8890", "Boeing 747", num_rows=22, num_seets_pr_row=6))
 print("This is airplane data:\n{0}\n{1}\n{2}".format(info.exportcleanvalue(), info.number(), info.exportcleanvalue()))
 
 #Choose seats
+
+
+
+
 
 
 #Print boardingcard
